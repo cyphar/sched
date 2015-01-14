1375ae7366b93c425f4c5f2f18ef490010ed2184
Aleksa Sarai <cyphar@cyphar.com>
2015-01-14 19:59:17 +1100

    main: update example

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
+#include "async.h"
 #include "sched.h"
 
 struct sched_t scheduler;
@@ -9,86 +11,28 @@ struct sched_t scheduler;
 /* NOTE: ARDUINO EMULATOR */
 /* ====================== */
 
-bool on = true;
-bool off = false;
 
-void led(bool *state) {
-	bool *next = NULL;
-
-	switch(*state) {
-		case true:
-			next = &off;
-			printf("explicit: on\n");
-			break;
-		case false:
-			next = &on;
-			printf("explicit: off\n");
-			break;
-	}
-
-	struct task_t task;
-	task_clear(&task);
-	task.task = (task_fp) led,
-	task.task_arg = next,
-	task.mtime = 500,
-	task.flag = ONCE,
-	sched_register(&scheduler, task);
-}
-
-void toggle_led(void *_) {
-	printf("toggle: on\n");
-	printf("toggle: off\n");
-}
-
-void idle(void *_) {
-	/*printf("idling!\n");*/
-
-	/* wait 100ms when idling */
-	/*
-	long mnow = __tasks_get_mtime();
-	while(__tasks_get_mtime() < mnow + 5)
-		;
-	*/
-
-	struct task_t task;
-	task_clear(&task);
-	task.task = (task_fp) idle,
-	task.mtime = 5,
-	task.flag = IDLE,
-	sched_register(&scheduler, task);
-}
 
 void setup(void) {
-	struct task_t task;
 	sched_init(&scheduler);
 
-	task_clear(&task);
-	task.task = (task_fp) led,
-	task.task_arg = &on,
-	task.mtime = 500,
-	task.flag = ONCE,
-	sched_register(&scheduler, task);
+	struct tone_t *tone440 = malloc(sizeof(struct tone_t));
+	tone_clear(tone440);
+	tone440->pin = 13;
+	tone440->amplitude = 0.5;
+	tone440->frequency = 440;
+	tone440->start = 0;
+	tone440->end = 3000;
+	async_tone(&scheduler, tone440);
 
-	task_clear(&task);
-	task.task = toggle_led,
-	task.task_arg = NULL,
-	task.mtime = 300,
-	task.flag = PERIODIC,
-	sched_register(&scheduler, task);
-
-	task_clear(&task);
-	task.task = toggle_led,
-	task.task_arg = NULL,
-	task.mtime = 350,
-	task.flag = PERIODIC,
-	sched_register(&scheduler, task);
-
-	task_clear(&task);
-	task.task = idle,
-	task.task_arg = NULL,
-	task.mtime = 0,
-	task.flag = IDLE,
-	sched_register(&scheduler, task);
+	struct tone_t *tone880 = malloc(sizeof(struct tone_t));
+	tone_clear(tone880);
+	tone880->pin = 13;
+	tone880->amplitude = 0.6;
+	tone880->frequency = 880;
+	tone880->start = 1000;
+	tone880->end = 2000;
+	async_tone(&scheduler, tone880);
 }
 
 void loop(void) {
